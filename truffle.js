5af4a9ad071ad28819d4c70437a73560513b54fb
himansh123 <kriplani.himanshu96@gmail.com>
2018-03-03 15:57:58 +0530

    Add files via upload

--- truffle.js
+++ truffle.js
@@ -2,12 +2,11 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
-      network_id: "*" ,
-      // Match any network id
-      gas: 4600000
-
+      port: 7545,
+      network_id: "5777", // Match any network id
+      gas: 3000000
     }
-  }
+  },
+  //contracts_build_directory: "./dist/contracts", // disabled until fix for https://github.com/trufflesuite/truffle-migrate/issues/10
+  test_directory: "./test/contract"
 };
-
